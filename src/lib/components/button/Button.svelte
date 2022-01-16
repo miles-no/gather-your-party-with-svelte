aef56fbd3e70013c5095e26ae1c683a3c8f2c02e
Løvgren, Dag Erik <dagerikhl@gmail.com>
2022-01-16 21:37:31 +0100

    Extract Button component, clean up CSS, fix error handling in creator

---
+++ src/lib/components/button/Button.svelte
@@ -0,0 +1,16 @@
+<button class="button" on:click><slot/></button>
+
+<style>
+  .button {
+    height: var(--interactive-height);
+    width: var(--interactive-width);
+    border: var(--interactive-border);
+    border-radius: var(--interactive-border-radius);
+    cursor: pointer;
+  }
+  .button:focus,
+  .button:hover {
+    font-weight: bold;
+    box-shadow: 0 0 2px 1px gray;
+  }
+</style>
--- src/routes/creator/index.svelte
+++ src/routes/creator/index.svelte
@@ -1,14 +1,12 @@
-<script context="module" lang="ts">
-</script>
-
 <script lang="ts">
+	import Button from '$lib/components/button/Button.svelte';
 	import { RACES } from '$lib/models/races';
 	import type { Race } from '$lib/types/race';
 
 	let name = '';
 
 	type RaceOption = { id: Race; name: string };
-	let raceOptions: RaceOption[] = [
+	let RACE_OPTIONS: RaceOption[] = [
 		{ id: RACES.Human, name: 'Human' },
 		{ id: RACES.Elf, name: 'Elf' },
 		{ id: RACES.Dwarf, name: 'Dwarf' },
@@ -16,18 +14,33 @@
 	];
 	let race = RACES.Human;
 
-	let submissionStatus: string;
+	type SubmissionStatus = 'success' | 'failure';
+	const SUBMISSION_STATUSES: { [key: string]: SubmissionStatus } = {
+		Success: 'success',
+		Failure: 'failure',
+	};
+	let submissionStatus: SubmissionStatus;
+
+	const clearSubmissionStatusAfterDelay = (delay = 5000) => {
+		setTimeout(() => {
+			submissionStatus = undefined;
+		}, delay);
+	};
 
 	const handleSubmit = () => {
-		fetch('/api/characters', { method: 'POST', body: JSON.stringify({ name, race }) })
-			.then(() => {
-				submissionStatus = 'success';
-			})
-			// TODO Fix that errors doesn't result in a catch
-			.catch(() => {
-				console.log('ERROR!');
-				submissionStatus = 'failure';
-			});
+		fetch('/api/characters', { method: 'POST', body: JSON.stringify({ name, race }) }).then(
+			async (response) => {
+				if (response.status >= 400) {
+					console.error(await response.text());
+
+					submissionStatus = SUBMISSION_STATUSES.Failure;
+					clearSubmissionStatusAfterDelay();
+				} else {
+					submissionStatus = SUBMISSION_STATUSES.Success;
+					clearSubmissionStatusAfterDelay();
+				}
+			},
+		);
 	};
 </script>
 
@@ -44,31 +57,22 @@
 	<label class="input select-input">
 		<span>Race</span>
 		<select bind:value={race}>
-			{#each raceOptions as { id, name }}
+			{#each RACE_OPTIONS as { id, name }}
 				<option value={id}>{name}</option>
 			{/each}
 		</select>
 	</label>
 
-	<button class="button" on:click={handleSubmit}>Save</button>
+	<Button on:click={handleSubmit}>Save</Button>
 
-	{#if submissionStatus === 'success'}
+	{#if submissionStatus === SUBMISSION_STATUSES.Success}
 		Character saved!
-	{:else if submissionStatus === 'failure'}
+	{:else if submissionStatus === SUBMISSION_STATUSES.Failure}
 		Error saving character
 	{/if}
 </section>
 
 <style>
-	:root {
-		--interactive-height: 40px;
-		--interactive-width: 12rem;
-		--interactive-padding-y: 0;
-		--interactive-padding-x: 0.5rem;
-		--interactive-border: 2px solid black;
-		--interactive-border-radius: 4px;
-	}
-
 	.container {
 		display: flex;
 		flex-direction: column;
@@ -94,33 +98,9 @@
 		font-size: 100%;
 	}
 
-	.text-input {
-	}
-	.text-input > span {
-	}
-	.text-input > input {
-	}
-
-	.select-input {
-	}
-	.select-input > span {
-	}
 	.select-input > select {
 		height: calc(var(--interactive-height) + 4px);
 		width: calc(var(--interactive-width) + 1.25rem);
 		padding: var(--interactive-padding-y) calc(var(--interactive-padding-x) - 0.15rem);
-	}
-
-	.button {
-		height: var(--interactive-height);
-		width: var(--interactive-width);
-		border: var(--interactive-border);
-		border-radius: var(--interactive-border-radius);
-		cursor: pointer;
-	}
-	.button:focus,
-	.button:hover {
-		font-weight: bold;
-		box-shadow: 0 0 2px 1px gray;
 	}
 </style>
