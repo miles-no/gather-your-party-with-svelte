84acadccdea57dd7df8a0fb497ca19ab46c0e090
Alexander Castillo <p.alexcast@gmail.com>
2022-02-08 17:17:50 +0100

    Set value when randomizing and use value instead of springed value for disableMin
    Using the value instead makes it disable the button instantly

--- src/lib/components/attribute-overview/AttributeOverview.svelte
+++ src/lib/components/attribute-overview/AttributeOverview.svelte
@@ -1,95 +1,96 @@
 <script lang="ts">
-	import AttributePicker from '$lib/components/attribute-picker/AttributePicker.svelte';
-	import { AttributeAllocation } from '$lib/types/attribute-allocation';
-	import type { Attribute } from '$lib/types/attribute';
-	import { ATTRIBUTES } from '$lib/models/attributes';
-	import { spring } from 'svelte/motion';
+    import AttributePicker from '$lib/components/attribute-picker/AttributePicker.svelte';
+    import {AttributeAllocation} from '$lib/types/attribute-allocation';
+    import type {Attribute} from '$lib/types/attribute';
+    import {ATTRIBUTES} from '$lib/models/attributes';
+    import {spring} from 'svelte/motion';
 
-	const STARTING_VALUE = 8;
-	const POINTS_TO_DISTRIBUTE = 20;
+    const STARTING_VALUE = 8;
+    const POINTS_TO_DISTRIBUTE = 20;
 
-	const initialAllocation: AttributeAllocation = {
-		[ATTRIBUTES.Charisma]: STARTING_VALUE,
-		[ATTRIBUTES.Constitution]: STARTING_VALUE,
-		[ATTRIBUTES.Intelligence]: STARTING_VALUE,
-		[ATTRIBUTES.Dexterity]: STARTING_VALUE,
-		[ATTRIBUTES.Strength]: STARTING_VALUE,
-		[ATTRIBUTES.Wisdom]: STARTING_VALUE,
-	};
+    const initialAllocation: AttributeAllocation = {
+        [ATTRIBUTES.Charisma]: STARTING_VALUE,
+        [ATTRIBUTES.Constitution]: STARTING_VALUE,
+        [ATTRIBUTES.Intelligence]: STARTING_VALUE,
+        [ATTRIBUTES.Dexterity]: STARTING_VALUE,
+        [ATTRIBUTES.Strength]: STARTING_VALUE,
+        [ATTRIBUTES.Wisdom]: STARTING_VALUE,
+    };
 
-	export let id: string = undefined;
+    export let id: string = undefined;
 
-	export let value: AttributeAllocation = { ...initialAllocation };
+    export let value: AttributeAllocation = {...initialAllocation};
 
-	let pointsRemaining = POINTS_TO_DISTRIBUTE;
+    let pointsRemaining = POINTS_TO_DISTRIBUTE;
 
-	let displayedAttributes = spring(value, {
-		stiffness: 0.2,
-	});
+    let displayedAttributes = spring(value, {
+        stiffness: 0.2,
+    });
 
-	let displayedPointsRemaining = spring(pointsRemaining, {
-		stiffness: 0.2,
-	});
+    let displayedPointsRemaining = spring(pointsRemaining, {
+        stiffness: 0.2,
+    });
 
-	const subtractPoint = (key: Attribute) => {
-		if (pointsRemaining >= POINTS_TO_DISTRIBUTE || value[key] <= STARTING_VALUE) {
-			return;
-		}
-		value[key] -= 1;
-		pointsRemaining++;
-		displayedPointsRemaining.set(pointsRemaining);
-		displayedAttributes.set(value);
-	};
+    const subtractPoint = (key: Attribute) => {
+        if (pointsRemaining >= POINTS_TO_DISTRIBUTE || value[key] <= STARTING_VALUE) {
+            return;
+        }
+        value[key] -= 1;
+        pointsRemaining++;
+        displayedPointsRemaining.set(pointsRemaining);
+        displayedAttributes.set(value);
+    };
 
-	const addPoint = (key: Attribute) => {
-		if (pointsRemaining <= 0) {
-			return;
-		}
-		value[key] += 1;
-		pointsRemaining--;
-		displayedPointsRemaining.set(pointsRemaining);
-		displayedAttributes.set(value);
-	};
+    const addPoint = (key: Attribute) => {
+        if (pointsRemaining <= 0) {
+            return;
+        }
+        value[key] += 1;
+        pointsRemaining--;
+        displayedPointsRemaining.set(pointsRemaining);
+        displayedAttributes.set(value);
+    };
 
-	const randomize = () => {
-		const allocation = { ...initialAllocation };
-		const attributeNames = Object.values(ATTRIBUTES);
-		for (let count = 0; count < POINTS_TO_DISTRIBUTE; count++) {
-			const randomAttribute = attributeNames[(Math.random() * attributeNames.length) | 0];
-			allocation[randomAttribute]++;
-			pointsRemaining--;
-		}
-		pointsRemaining = 0;
-		displayedPointsRemaining.set(pointsRemaining);
-		displayedAttributes.set(allocation);
-	};
+    const randomize = () => {
+        const allocation = {...initialAllocation};
+        const attributeNames = Object.values(ATTRIBUTES);
+        for (let count = 0; count < POINTS_TO_DISTRIBUTE; count++) {
+            const randomAttribute = attributeNames[(Math.random() * attributeNames.length) | 0];
+            allocation[randomAttribute]++;
+            pointsRemaining--;
+        }
+        pointsRemaining = 0;
+        value = allocation;
+        displayedPointsRemaining.set(pointsRemaining);
+        displayedAttributes.set(allocation);
+    };
 </script>
 
 <div class="container" {id}>
-	<span>Points to distribute: {Math.round($displayedPointsRemaining)}</span>
-	<button class="randomize-btn rpgui-button rpgui-button--small" on:click={randomize}
-		>Randomize
-	</button>
-	{#each Object.entries($displayedAttributes) as [name, value]}
-		<AttributePicker
-			{name}
-			value={Math.round(value)}
-			disableMin={STARTING_VALUE === value}
-			disableMax={0 === pointsRemaining}
-			on:subtract={() => subtractPoint(name)}
-			on:add={() => addPoint(name)}
-		/>
-	{/each}
+    <span>Points to distribute: {Math.round($displayedPointsRemaining)}</span>
+    <button class="randomize-btn rpgui-button rpgui-button--small" on:click={randomize}
+    >Randomize
+    </button>
+    {#each Object.entries($displayedAttributes) as [name, attributeValue]}
+        <AttributePicker
+                {name}
+                value={Math.round(attributeValue)}
+                disableMin={STARTING_VALUE === value[name]}
+                disableMax={0 === pointsRemaining}
+                on:subtract={() => subtractPoint(name)}
+                on:add={() => addPoint(name)}
+        />
+    {/each}
 </div>
 
 <style>
-	.container {
-		display: flex;
-		flex-direction: column;
-	}
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
 
-	.randomize-btn {
-		margin: 0.5em 0;
-		width: 275px;
-	}
+    .randomize-btn {
+        margin: 0.5em 0;
+        width: 275px;
+    }
 </style>
