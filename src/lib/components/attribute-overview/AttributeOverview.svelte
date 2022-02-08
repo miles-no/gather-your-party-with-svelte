345d2988823b9f9dd86eb4b710d2010eebccc95c
Alexander Castillo <p.alexcast@gmail.com>
2022-02-08 17:14:52 +0100

    Add attribute randomizer with spring

--- src/lib/components/attribute-overview/AttributeOverview.svelte
+++ src/lib/components/attribute-overview/AttributeOverview.svelte
@@ -3,13 +3,12 @@
 	import { AttributeAllocation } from '$lib/types/attribute-allocation';
 	import type { Attribute } from '$lib/types/attribute';
 	import { ATTRIBUTES } from '$lib/models/attributes';
+	import { spring } from 'svelte/motion';
 
 	const STARTING_VALUE = 8;
 	const POINTS_TO_DISTRIBUTE = 20;
 
-	export let id: string = undefined;
-
-	export let value: AttributeAllocation = {
+	const initialAllocation: AttributeAllocation = {
 		[ATTRIBUTES.Charisma]: STARTING_VALUE,
 		[ATTRIBUTES.Constitution]: STARTING_VALUE,
 		[ATTRIBUTES.Intelligence]: STARTING_VALUE,
@@ -18,31 +17,63 @@
 		[ATTRIBUTES.Wisdom]: STARTING_VALUE,
 	};
 
+	export let id: string = undefined;
+
+	export let value: AttributeAllocation = { ...initialAllocation };
+
 	let pointsRemaining = POINTS_TO_DISTRIBUTE;
+
+	let displayedAttributes = spring(value, {
+		stiffness: 0.2,
+	});
+
+	let displayedPointsRemaining = spring(pointsRemaining, {
+		stiffness: 0.2,
+	});
 
 	const subtractPoint = (key: Attribute) => {
 		if (pointsRemaining >= POINTS_TO_DISTRIBUTE || value[key] <= STARTING_VALUE) {
 			return;
 		}
-		value[key]--;
+		value[key] -= 1;
 		pointsRemaining++;
+		displayedPointsRemaining.set(pointsRemaining);
+		displayedAttributes.set(value);
 	};
 
 	const addPoint = (key: Attribute) => {
 		if (pointsRemaining <= 0) {
 			return;
 		}
-		value[key]++;
+		value[key] += 1;
 		pointsRemaining--;
+		displayedPointsRemaining.set(pointsRemaining);
+		displayedAttributes.set(value);
+	};
+
+	const randomize = () => {
+		const allocation = { ...initialAllocation };
+		const attributeNames = Object.values(ATTRIBUTES);
+		for (let count = 0; count < POINTS_TO_DISTRIBUTE; count++) {
+			const randomAttribute = attributeNames[(Math.random() * attributeNames.length) | 0];
+			allocation[randomAttribute]++;
+			pointsRemaining--;
+		}
+		pointsRemaining = 0;
+		displayedPointsRemaining.set(pointsRemaining);
+		displayedAttributes.set(allocation);
 	};
 </script>
 
 <div class="container" {id}>
-	<span>Points to distribute: {pointsRemaining}</span>
-	{#each Object.entries(value) as [name, value]}
+	<span>Points to distribute: {Math.round($displayedPointsRemaining)}</span>
+	<button class="randomize-btn rpgui-button rpgui-button--small" on:click={randomize}
+		>Randomize
+	</button>
+	{#each Object.entries($displayedAttributes) as [name, value]}
 		<AttributePicker
 			{name}
-			{value}
+			value={Math.round(value)}
 			disableMin={STARTING_VALUE === value}
 			disableMax={0 === pointsRemaining}
 			on:subtract={() => subtractPoint(name)}
@@ -50,3 +81,15 @@
 		/>
 	{/each}
 </div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.randomize-btn {
+		margin: 0.5em 0;
+		width: 275px;
+	}
+</style>
