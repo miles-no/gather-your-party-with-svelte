1ed8abf990bb102a052a17898f1137bf280addc1
Alexander Castillo <p.alexcast@gmail.com>
2022-02-09 15:13:21 +0100

    Only load character list client side

--- src/routes/characters/index.svelte
+++ src/routes/characters/index.svelte
@@ -1,43 +1,46 @@
 <script lang="ts">
-	import { ApiService, RESPONSE_STATUSES } from '$lib/utils/ApiService';
-	import { Character } from '$lib/types/character';
-	import CharacterPreviewList from '$lib/components/character-preview-list/CharacterPreviewList.svelte';
-	import Loader from '$lib/components/loader/Loader.svelte';
+    import {ApiService, RESPONSE_STATUSES} from '$lib/utils/ApiService';
+    import {Character} from '$lib/types/character';
+    import CharacterPreviewList from '$lib/components/character-preview-list/CharacterPreviewList.svelte';
+    import Loader from '$lib/components/loader/Loader.svelte';
+    import {onMount} from "svelte";
 
-	const fetchCharacters = new ApiService<Character[]>();
-	const isLoading = fetchCharacters.isLoading;
-	const responseStatus = fetchCharacters.responseStatus;
-	const characters = fetchCharacters.response;
+    const fetchCharacters = new ApiService<Character[]>();
+    const isLoading = fetchCharacters.isLoading;
+    const responseStatus = fetchCharacters.responseStatus;
+    const characters = fetchCharacters.response;
 
-	fetchCharacters.fetch('/api/characters').catch((error) => {
-		console.error(error);
-	});
+    onMount(async () => {
+        fetchCharacters.fetch('/api/characters').catch((error) => {
+            console.error(error);
+        });
+    });
 </script>
 
 <svelte:head>
-	<title>Characters</title>
+    <title>Characters</title>
 </svelte:head>
 
 <section class="rpgui-container framed outer-container" class:loading={$isLoading}>
-	{#if $isLoading}
-		<Loader />
-	{:else if $responseStatus === RESPONSE_STATUSES.Success}
-		<CharacterPreviewList characters={$characters} />
-	{:else if $responseStatus === RESPONSE_STATUSES.Failure}
-		&cross; Error getting characters, see console for more info.
-	{/if}
+    {#if $isLoading}
+        <Loader/>
+    {:else if $responseStatus === RESPONSE_STATUSES.Success}
+        <CharacterPreviewList characters={$characters}/>
+    {:else if $responseStatus === RESPONSE_STATUSES.Failure}
+        &cross; Error getting characters, see console for more info.
+    {/if}
 </section>
 
 <style>
-	.outer-container {
-		display: flex;
-		flex-direction: column;
-		flex-grow: 1;
-		align-items: center;
-		gap: 1rem;
-	}
+    .outer-container {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        align-items: center;
+        gap: 1rem;
+    }
 
-	.loading {
-		justify-content: center;
-	}
+    .loading {
+        justify-content: center;
+    }
 </style>
