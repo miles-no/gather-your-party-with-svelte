b8b297328937c74a775ff42a900fbacc16e552af
Løvgren, Dag Erik <dagerikhl@gmail.com>
2022-02-11 17:54:44 +0100

    Another solution to the footer problem

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,62 +1,42 @@
 <footer class="grid">
-	<div class="center">
+	<div class="group">
 		<a href="https://github.com/miles-no/booster2022-gather-your-party-with-svelte" target="_blank"
 			>GitHub</a
 		>
-		<div>|</div>
+
+		<div class="separator">|</div>
+
 		<a href="https://svelte.dev/tutorial/basics" target="_blank">Svelte Docs</a>
-		<div>|</div>
+
+		<div class="separator">|</div>
+
 		<a href="https://kit.svelte.dev/docs" target="_blank">SvelteKit Docs</a>
 	</div>
-	<div class="right">
+
+	<div class="group">
 		<a href="https://github.com/dagerikhl" target="_blank">dagerikhl@github</a>
+
+		<div class="separator">|</div>
+
 		<a href="https://github.com/palexcast" target="_blank">palexcast@github</a>
 	</div>
 </footer>
 
 <style>
 	footer {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		grid-template-rows: 1fr;
-		gap: 0.25rem;
-		margin-bottom: 1rem;
+		display: flex;
+		justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 2rem;
+    margin: 1rem;
 	}
 
-	footer a {
-		margin: 0 0.5em;
-	}
-
-	.center {
+	.group {
 		display: flex;
 		align-items: center;
-		justify-content: center;
-		grid-column: 2;
-		grid-row: 1;
 	}
 
-	.right {
-		display: flex;
-		align-items: center;
-		justify-content: flex-end;
-		grid-column: 3;
-		grid-row: 1;
-	}
-
-	@media (max-width: 1300px) {
-		footer {
-			grid-template-rows: 1fr 1fr;
-		}
-
-		.center {
-			grid-column: 1/4;
-			grid-row: 1;
-		}
-
-		.right {
-			justify-content: center;
-			grid-column: 1/4;
-			grid-row: 2;
-		}
+	.separator {
+		margin: 0 0.5rem;
 	}
 </style>
