81a06f9c611ecc950e2124751b0706ce1c735b15
Alexander Castillo <p.alexcast@gmail.com>
2022-02-15 16:38:07 +0100

    Add quests and update readme

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export const getSession = (): Session => {
 	const readme = fs.readFileSync(path.resolve('README.md'), { encoding: 'utf8' });
+	const quests = fs.readFileSync(path.resolve('workshop-tasks.md'), { encoding: 'utf8' });
 
-	return { readme };
+	return { readme, quests };
 };
