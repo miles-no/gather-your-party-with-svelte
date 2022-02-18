fc99616fe344be51154bad1c904d5334e63de0a1
Alexander Castillo <p.alexcast@gmail.com>
2022-02-18 15:12:57 +0100

    Rework hints

--- hints.js
+++ hints.js
@@ -1,26 +1,19 @@
 import * as cowsay from 'cowsay';
 
-const hints = [
-	'Hint number 1 will help you!',
-	'Hint number 2 will not help you so much, but it is longer and a lot cooler 🆒\nIt also includes line breaks which are\nReally\ncool\nHere is a link\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ',
-];
+const hints = {
+    '1a': 'Hint number 1 will help you!',
+    '2a': 'Hint number 2 will not help you so much, but it is longer and a lot cooler 🆒\nIt also includes line breaks which are\nReally\ncool\nHere is a link\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ',
+}
 
 const args = process.argv.slice(2);
 
 const hintArg = args[0];
-const hintNr = +hintArg;
 
 if (hintArg === undefined) {
-	console.error('ERROR: Hint number must be specified. Example `npm run hint 1`.');
-} else if (Number.isNaN(hintNr)) {
-	console.error('ERROR: Hint number must be a number. Example `npm run hint 1`.');
-} else if (hintNr <= 0 || hintNr > hints.length) {
-	console.error(
-		'ERROR: Invalid hint range, must be between 1 - ' +
-			hints.length +
-			'. Example `npm run hint 1`.',
-	);
+    console.error('ERROR: Hint id must be specified. Example `npm run hint 1a`.');
+} else if (!hints[hintArg]) {
+    console.error(`ERROR: Could not find hint ${hintArg}. Example \`npm run hint 1a\`.`);
 } else {
-	const output = cowsay.say({ text: hints[hintNr - 1] });
-	console.log(output);
+    const output = cowsay.say({text: hints[hintArg]});
+    console.log(output);
 }
