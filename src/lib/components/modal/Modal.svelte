bc4194fd79ca5406de9ee81524aedff4c99b913a
Løvgren, Dag Erik <dagerikhl@gmail.com>
2022-03-03 13:26:16 +0100

    Add back not-part-of-workshop comment in Modal

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	/***
+	 * NOT PART OF WORKSHOP
+	 *
+	 * It should not be necessary to familiarize oneself with this file for the workshop.
+	 *
+	 * This file contains logic for creating a modal overlaying the screen with an optional
+	 * title and some content.
+	 */
+
 	import { createEventDispatcher } from 'svelte';
 	import Button from '$lib/components/button/Button.svelte';
 
