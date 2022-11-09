1111cda8d5b7f6f8bdf7e9d95936a9449bd62335
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-11-09 22:00:30 +0100

    Revert move of portraits

--- src/routes/api/portraits/[id]/+server.ts
+++ src/routes/api/portraits/[id]/+server.ts
@@ -7,15 +7,13 @@
  */
 
 import type { RequestHandler } from '@sveltejs/kit';
-// import fs from 'fs';
+import fs from 'fs';
 
 // GET /api/portraits/[id]
-export const GET: RequestHandler = async ({ fetch, params }) => {
+export const GET: RequestHandler = async ({ params }) => {
 	const lookupId = params.id;
-	// const path = `src/lib/_workshop-internals/assets/portraits/${lookupId}`;
-	// const data = fs.readFileSync(path, 'base64');
+	const path = `src/lib/_workshop-internals/assets/portraits/${lookupId}`;
+	const data = fs.readFileSync(path, 'base64');
 
-	return fetch(`/assets/portraits/${lookupId}`);
-
-	// return new Response(`"data:image/jpeg;base64,${data}"`);
+	return new Response(`"data:image/jpeg;base64,${data}"`);
 };
