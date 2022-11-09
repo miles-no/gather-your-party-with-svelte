c759ccd30c945c37ca28b6275781c8de19435175
Dag Erik Løvgren <dagerikhl@gmail.com>
2022-11-09 21:50:03 +0100

    Move portraits to static dir

--- src/routes/api/portraits/[id]/+server.ts
+++ src/routes/api/portraits/[id]/+server.ts
@@ -7,13 +7,15 @@
  */
 
 import type { RequestHandler } from '@sveltejs/kit';
-import fs from 'fs';
+// import fs from 'fs';
 
 // GET /api/portraits/[id]
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ fetch, params }) => {
 	const lookupId = params.id;
-	const path = `src/lib/_workshop-internals/assets/portraits/${lookupId}`;
-	const data = fs.readFileSync(path, 'base64');
+	// const path = `src/lib/_workshop-internals/assets/portraits/${lookupId}`;
+	// const data = fs.readFileSync(path, 'base64');
 
-	return new Response(`"data:image/jpeg;base64,${data}"`);
+	return fetch(`/assets/portraits/${lookupId}`);
+
+	// return new Response(`"data:image/jpeg;base64,${data}"`);
 };
